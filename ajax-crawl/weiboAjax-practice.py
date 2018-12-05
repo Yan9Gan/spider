292148d780932bd6ece0b4a760ff8a2011064659
aisong_data_processing <yg247350078@163.com>
2018-12-05 14:28:12 +0800

    微博ajax爬取练习

---
+++ ajax-crawl/weiboAjax-practice.py
@@ -0,0 +1,58 @@
+from urllib.parse import urlencode
+import requests
+from pyquery import PyQuery as pq
+
+
+base_url = 'https://m.weibo.cn/api/container/getIndex?'
+headers = {
+    'Host': 'm.weibo.cn',
+    'Referer': 'https://m.weibo.cn/u/2830678474',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+}
+
+
+def get_page(page):
+    params = {
+        'type': 'uid',
+        'value': '2830678474',
+        'containerid': '1076032830678474',
+        'page': page
+    }
+    url = base_url + urlencode(params)
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+    except requests.ConnectionError as e:
+        print('Error', e.args)
+
+
+def parse_page(json):
+    if json:
+        items = json.get('data').get('cards')
+        for item in items:
+            item = item.get('mblog')
+            if item:
+                weibo = {}
+                weibo['id'] = item.get('id')
+                weibo['text'] = item.get('text')
+                weibo['attitudes'] = item.get('attitudes_count')
+                weibo['comments'] = item.get('comments_count')
+                weibo['reposts'] = item.get('reposts_count')
+                yield weibo
+
+
+if __name__ == '__main__':
+    for page in range(1, 11):
+        json = get_page(page)
+        results = parse_page(json)
+        for result in results:
+            # TODO：保存数据到MongoDB
+            print(result)
+
+
+
+
+
+
