f6c4b08c9e58b68f7aa73d182576dc6352bc83f5
aisong_data_processing <yg247350078@163.com>
2018-12-07 15:44:36 +0800

    爬取360搜图，保存图片并将对应数据保存至mysql

---
+++ Scrapy/images360/images360/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy import Item, Field
+
+
+class ImageItem(scrapy.Item):
+    collection = table = 'images'
+    id = Field()
+    url = Field()
+    title = Field()
+    thumb = Field()
