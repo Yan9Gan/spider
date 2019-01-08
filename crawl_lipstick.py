51aa2a35a3e95e54392dabbcdbf976f90b4ed8f5
aisong_data_processing <yg247350078@163.com>
2019-01-08 18:44:13 +0800

    改用京东爬口红

--- crawl_lipstick.py
+++ crawl_lipstick.py
@@ -3,46 +3,56 @@ from urllib.parse import urlencode
 from bs4 import BeautifulSoup
 
 
-base_url = 'https://s.taobao.com/search?'
-
+base_url = 'https://search.jd.com/search?'
 params = {
-    'q': '口红',
-    'imgfile': '',
-    'commend': 'all',
-    'ssid': 's5-e',
-    'search_type': 'item',
-    'sourceId': 'tb.index',
-    'spm': 'a21bo.2017.201856-taobao-item.1',
-    'ie': 'utf8',
-    'initiative_id': 'tbindexz_20170306',
+    'keyword': '口红',
+    'enc': 'utf-8',
+    'wq': '口红',
+    'pvid': 'e2d50b628f024c3ab7305f29fedc5c4e',
 }
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-    'referer': 'https://www.taobao.com/',
-    'cookie': 'miid=452902621275025248; t=473d8363b7f9fdfc007888a9f9548959; hng=CN%7Czh-CN%7CCNY%7C156; cna=t/NnFDBVdmoCAXjFABBQ6qZE; thw=cn; cookie2=1a225b51d183ffcf66c89c0fe145e8d2; _tb_token_=e8b8b0d49ff68; tg=0; enc=jFpzZ9MkDLM5rSDROUaZG7QVpPZ1B7irt999nVBdYz5KcvMvDllcUhHX1Y97btlHtb5%2B1wr68ptExGBzGdMJnA%3D%3D; swfstore=92392; x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; v=0; alitrackid=www.taobao.com; unb=3853197365; sg=955; _l_g_=Ug%3D%3D; skt=14f3188d8db55436; cookie1=BxUMXSIyZ4uf0TmhOrmfkhewaEOj9NBVUL9BHje%2Bgwk%3D; csg=a7fad350; uc3=vt3=F8dByRIpaiGtpf6TYX0%3D&id2=UNiBSepnt%2FkWSg%3D%3D&nk2=F5RCZV45vAU5vw%3D%3D&lg2=Vq8l%2BKCLz3%2F65A%3D%3D; existShop=MTU0NjkzMjYzNw%3D%3D; tracknick=tb70642839; lgc=tb70642839; _cc_=UtASsssmfA%3D%3D; dnk=tb70642839; _nk_=tb70642839; cookie17=UNiBSepnt%2FkWSg%3D%3D; JSESSIONID=30D9B282CDAEA8C7ED499DC4D782B2FA; lastalitrackid=login.taobao.com; uc1=cookie16=UIHiLt3xCS3yM2h4eKHS9lpEOw%3D%3D&cookie21=WqG3DMC9Fb5mPLIQo9kR&cookie15=VFC%2FuZ9ayeYq2g%3D%3D&existShop=false&pas=0&cookie14=UoTYMDKeyGiQuQ%3D%3D&tag=8&lng=zh_CN; mt=ci=3_1; l=aBV5LjD0yiLvm0QXQMaigX_D77076CBPehMF1MaklTEhNPV17RXy17no-Vw6j_qC51cy_K-5F; isg=BEdHqwKT1-o08lNVgc7lpivC1vvR5BlKfcV6NRk0blb9iGdKIR_of6HKLghzYPOm; whl=-1%260%260%261546932645238'
+    'referer': 'https://www.jd.com/?cu=true&utm_source=baidu-search&utm_medium=cpc&utm_campaign=t_262767352_baidusearch&utm_term=106807362512_0_8529cea1a94b49f4a3c590484b92f623',
+    'cookie': 'shshshfpa=953e8d25-60e0-b22f-1257-709613744249-1544175519; xtest=3849.cf6b6759; cn=0; qrsc=3; __jdu=1544175519090151046347; shshshfpb=jfZWHxwyMOLD5NpNK7jXlHQ%3D%3D; PCSYCityID=1601; ipLoc-djd=1-72-2799-0; unpl=V2_ZzNtbUtTQB0nXBZUKxFZVmILRVgSVRERfQlDXX5KVAYwBBFfclRCFX0UR1RnGFUUZwoZXkZcQxxFCEdkeB5fA2AFEFlBZxBFLV0CFi9JH1c%2bbRJcRV5CE3cPRVB7Gmw1ZAMiXUNnQxFwDUZSfx1cDWYLE11GV0IQcgpDUkspWzVXMxFcS1dGHEUJdlVLWwhZZQcXVUNeDhVxDUNUfR1YBW8CGlxCU0MUcA9EUX0pXTVk; __jda=122270672.1544175519090151046347.1544175519.1546933799.1546943120.12; __jdc=122270672; __jdv=122270672|baidu-search|t_262767352_baidusearch|cpc|106807362512_0_8529cea1a94b49f4a3c590484b92f623|1546943119539; __jdb=122270672.2.1544175519090151046347|12.1546943120; shshshfp=9547204bb990f25f550a6b237f272d82; shshshsID=6a85d710953b0e76022d6742dc39b7d2_2_1546943143292; rkv=V0900; 3AB9D23F7A4B3C9B=M7E73QMR2UZZGHIVSA6TXFUUOV7Y3AONXSZD5YN3PRW73FICPNSUKGIEZ2CHU4Y3ZBKPKA4P7G6XAJCFNMSQ5V2VNQ'
 }
+brand_url_list = []
 
 
 def get_first_page():
     url = base_url + urlencode(params)
-    print(url)
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.text
 
+    print("首页访问失败")
+    return None
 
-def parse_first_page(html):
+
+def get_brand_url(html):
     soup = BeautifulSoup(html, 'html5lib')
-    brand_codes = soup.select('#J_NavCommonRowItems_0 a')
-    for brand_code in brand_codes:
-        print(brand_code.get('data-value'))
+    brand_as = soup.select('#J_selector > div.J_selectorLine.s-brand > div > div.sl-value > div.sl-v-logos > ul > li')
+    for brand_a in brand_as:
+        brand_url = brand_a.find('a').get('href')
+        brand_url = base_url + brand_url[7:]
+        brand_url_list.append(brand_url)
+        return brand_url_list
+
+
+def get_per_page_url(brand_url):
+    response = requests.get(brand_url, headers=headers)
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html5lib')
+        pages = soup.select('#J_goodsList > ul > li')
+        for page in pages:
+            pass
+
+    print("访问" + brand_url + "失败")
 
 
 if __name__ == '__main__':
     html = get_first_page()
-    print(html)
-    parse_first_page(html)
+    brand_url_list = get_brand_url(html)
 
 
 
