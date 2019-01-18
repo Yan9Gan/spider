848138d28160562149d512f0f2f6fb9abdfd73a5
aisong_data_processing <yg247350078@163.com>
2019-01-18 15:30:28 +0800

    解析数据时打开url改用chrome()，增加几个数据的提取

--- jd_lipstick/parse_page.py
+++ jd_lipstick/parse_page.py
@@ -1,3 +1,4 @@
+import re
 from selenium import webdriver
 from pymongo import MongoClient
 
@@ -6,10 +7,11 @@ client = MongoClient()
 db = client['jdLipstick']
 collections_name = db.list_collection_names()
 
-browser = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
+# browser = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
 
 
 def parse_page(url):
+    browser = webdriver.Chrome()
     try:
         browser.get(url)
 
@@ -19,17 +21,40 @@ def parse_page(url):
     else:
         print(url)
         infos_div = browser.find_element_by_class_name('itemInfo-wrap')
+        # 标题
         title = infos_div.find_element_by_class_name('sku-name').get_attribute('innerText')
+        # 价格
         price = infos_div.find_element_by_class_name('p-price').get_attribute('innerText')
-        print(title, price)
+        print(title)
+        good_introduces = browser.find_element_by_class_name('parameter2')
+        # 商品毛重
+        gross_weight = good_introduces.find_element_by_xpath('./li[4]').text.split('：')[1]
+        # 妆效
+        makeup_effect = good_introduces.find_element_by_xpath('./li[5]').text.split('：')[1]
+        # 色系
+        color_system = good_introduces.find_element_by_xpath('./li[7]').text.split('：')[1]
+        # 产地
+        origin = good_introduces.find_element_by_xpath('./li[8]').text.split('：')[1]
+        print('价格：', price)
+        print('商品毛重：', gross_weight)
+        print('妆效：', makeup_effect)
+        print('色系：', color_system)
+        print('产地：', origin)
+
+    finally:
+        browser.close()
 
 
 if __name__ == '__main__':
+    sum = 0
     for collection_name in collections_name:
         collection = db[collection_name]
+        print(collection_name, ':', collection.count())
+        sum += collection.count()
         for data in collection.find():
             url = data.get('url')
             parse_page(url)
+    print(sum)
 
 
 
