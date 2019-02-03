a294be87ea2cc2faf906c4ebb23ee851e534207f
ghosque <yg247350078@163.com>
2019-02-03 15:55:50 +0800

    数据分析

---
+++ Scrapy/jdLipstick/jdLipstick/analysis/analysis.py
@@ -0,0 +1,44 @@
+import pyecharts
+from pymongo import MongoClient
+
+
+class Analysis(object):
+
+    def __init__(self):
+        self.client = MongoClient()
+        self.db = self.client['jd_lipstick']
+        self.collections = self.db.list_collection_names()
+
+    def show_all(self):
+        for collection_name in self.collections:
+            print(self.db[collection_name].find())
+
+    def average_price(self):
+        price_list = []
+        for collection_name in self.collections:
+            collection = self.db[collection_name]
+            sum = 0
+            for item in collection.find():
+                price = float(item['price'][1:])
+                sum += price
+            price_list.append(round(sum / collection.count(), 2))
+
+        bar = pyecharts.Bar('京东口红')
+        bar.add('各品牌平均价格', self.collections, price_list, is_more_utils=True)
+        bar.render('html/average.html')
+
+    def amount(self):
+        count_list = []
+        for collection_name in self.collections:
+            count_list.append(self.db[collection_name].count())
+
+        pie = pyecharts.Pie('京东口红')
+        pie.add('数量', self.collections, count_list, is_label_show=True)
+        pie.render('html/amount.html')
+
+
+if __name__ == '__main__':
+    al = Analysis()
+    al.amount()
+
+
